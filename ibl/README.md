4090da2206b2ac334dd114d4d9b86c70b0817061
Emin Orhan <aeminorhan@gmail.com>
2025-03-24 10:14:05 -0400

    add ibl

---
+++ ibl/README.md
@@ -0,0 +1,12 @@
+IBL dataset. Dataset URL: https://dandiarchive.org/dandiset/000409
+
+To download:
+```python
+dandi ls -r DANDI:000409 > log.txt
+grep 'path:' log.txt | awk '{print $2}' | grep 'processed-only' > processed_paths.txt
+sh download_ibl.sh
+```
+
+Token count: ??
+
+HF repo: https://huggingface.co/datasets/eminorhan/ibl
---
+++ ibl/create_ibl.py
@@ -0,0 +1,106 @@
+import os
+import argparse
+import numpy as np
+from pynwb import NWBHDF5IO
+from datasets import Dataset
+
+
+def find_nwb_files(root_dir):
+    """
+    Crawls through a directory (including subdirectories), finds all files
+    that end with ".nwb", and returns the full paths of all the found files in a list.
+
+    Args:
+        root_dir: The root directory to start the search from.
+
+    Returns:
+        A list of full paths to the found .nwb files, or an empty list if
+        no files are found or if the root directory is invalid.
+        Returns None if root_dir is not a valid directory.
+    """
+
+    if not os.path.isdir(root_dir):
+        print(f"Error: '{root_dir}' is not a valid directory.")
+        return None
+
+    nwb_files = []
+    for dirpath, _, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if filename.endswith(".nwb"):
+                full_path = os.path.join(dirpath, filename)
+                nwb_files.append(full_path)
+    return nwb_files
+
+
+def extract_file_identifier(file_path):
+    """
+    Extracts a file identifier string from a full file path.
+
+    Args:
+        file_path (str): The full file path.
+
+    Returns:
+        str: The full file identifier string.
+    """
+    directory, filename = os.path.split(file_path)
+    subdirectory = os.path.basename(directory)
+    filename_without_extension, _ = os.path.splitext(filename)
+    return f"{subdirectory}_{filename_without_extension}"
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('Consolidate data in multiple files into a single file', add_help=False)
+    parser.add_argument('--data_dir',default="data",type=str, help='Data directory')
+    return parser
+
+
+if __name__ == '__main__':
+
+    args = get_args_parser()
+    args = args.parse_args()
+    print(args)
+
+    # get all .nwb files in the sorted folder
+    nwb_files = find_nwb_files(args.data_dir)
+    print(f"Files: {nwb_files}")
+    print(f"Total number of files: {len(nwb_files)}")
+
+    # lists to store results for each session
+    spike_counts_list, identifier_list = [], []
+
+    # token counter
+    n_tokens = 0
+
+    for file_path in sorted(nwb_files):
+        print(f"Processing file: {file_path}")
+        with NWBHDF5IO(file_path, "r") as io:
+            nwbfile = io.read()
+
+            # we will save just spike activity for now
+            units = nwbfile.units.to_dataframe()
+            max_time = max([u.max() for u in units['spike_times']])
+            spike_counts = np.vstack([np.histogram(row, bins=np.arange(0, max_time + 0.02, 0.02))[0] for row in units['spike_times']]).astype(np.uint8)  # spike count matrix (nxt: n is #channels, t is time bins)
+
+            # file identifier
+            identifier = extract_file_identifier(file_path)
+
+            # append sessions
+            spike_counts_list.append(spike_counts)
+            identifier_list.append(identifier)
+
+            print(f"Spike count shape / max: {spike_counts.shape} / {spike_counts.max()}")
+            n_tokens += np.prod(spike_counts.shape)
+
+    def gen_data():
+        for a, b in zip(spike_counts_list, identifier_list):
+            yield {
+                "spike_counts": a,
+                "identifier": b,
+                }
+
+    ds = Dataset.from_generator(gen_data, writer_batch_size=1)
+    print(f"Estimated size of dataset: {ds._estimate_nbytes()/1e9:.2f} GB")
+    print(f"Number of tokens in dataset: {n_tokens} tokens")
+
+    # push all data to hub
+    ds.push_to_hub("eminorhan/ibl", num_shards=10, token=True)
